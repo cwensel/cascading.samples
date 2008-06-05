fef3c74cc3c208126de5e9ff9ad94f3b4405db70
cwensel <cwensel@72861afd-6043-0410-8b40-956228ca1d42>
2008-06-05 20:26:34 +0000

    updated copyright

--- logparser/src/java/logparser/Main.java
+++ logparser/src/java/logparser/Main.java
@@ -1,5 +1,22 @@
 /*
- * Copyright (c) 2008, Vinculum Technologies, Inc. All Rights Reserved.
+ * Copyright (c) 2007-2008 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Cascading is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cascading is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cascading.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package logparser;
@@ -55,7 +72,7 @@ public class Main
 
     // optionally print out the parsedLogFlow to a graph file for import into a graphics package
     // this is useful for visualizing the flow to help with debugging
-    //parsedLogFlow.writeDOT( "logparser.dot" );
+    parsedLogFlow.writeDOT( "logparser.dot" );
 
     // start execution of the flow (either locally or on the cluster
     parsedLogFlow.start();
